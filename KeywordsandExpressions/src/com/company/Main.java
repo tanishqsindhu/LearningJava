9eebfb7336829c2fbbf6d1023f1b4cf7d6ce0996
tanishqsindhu <hisar.champion@gmail.com>
2021-12-27 23:28:57 +0530

    Keyword and Expressions

---
+++ KeywordsandExpressions/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int int2=5;
+    //Expressions are building blocks of java
+        //a mile is equal to 1.609344 kilometer
+        double kilometer=(100*1.609344);
+        int highscore =50;
+        if(highscore==50) {//highscore==50 is expression
+        }
+        int score = 100;
+        if(score>99){
+            System.out.println("You got the high score!");
+            score =0;
+            //score =100,score>99,You got the high score!,score =0 are expressions
+        }
+    }
+}
