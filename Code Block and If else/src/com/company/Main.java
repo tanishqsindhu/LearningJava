6b7d3c282f11fe4dd6b0db0292c34ea5fc5a44cd
tanishqsindhu <hisar.champion@gmail.com>
2021-12-28 19:23:02 +0530

    Scope

--- Code Block and If else/src/com/company/Main.java
+++ Code Block and If else/src/com/company/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 4000;
+        int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
-        if (score == 5000)
-            System.out.println("Your score was 5000");
-        System.out.println("This was Executed");
+//        if (score < 5000 && score>1000){
+//            System.out.println("Your score was less than 5000");
+//        }
+//        else if (score < 1000) {
+//            System.out.println("Your score is less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+        if(gameOver){
+            int finalScore = score+(levelCompleted * bonus);
+            System.out.println("Your final score was"+ finalScore);
+        }
+        int savedFinalScore = finalScore;
     }
 }
