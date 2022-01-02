e68bc34c8c537ca9a40d4d0d0f3c1244fbfa8254
tanishqsindhu <hisar.champion@gmail.com>
2022-01-02 14:09:05 +0530

    alternative solution

--- Methods Challenge/src/com/company/Main.java
+++ Methods Challenge/src/com/company/Main.java
@@ -55,14 +55,25 @@ public class Main {
     public static void displayHighScorePosition(String playerName,int playerPosition){
         System.out.println(playerName+" managed to get into position "+playerPosition+" on the high score table");
     }
-    public static int calculateHighScorePosition(int playerScore){
-        if(playerScore>=1000){
-            return 1;
-        }else if(playerScore>=500){
-            return 2;
-        }else if(playerScore>=100){
-            return 3;
+        public static int calculateHighScorePosition(int playerScore){
+            if(playerScore>=1000){
+                return 1;
+            }else if(playerScore>=500){
+                return 2;
+            }else if(playerScore>=100){
+                return 3;
+            }
+            return 4;
+
+            //      Or
+//            int playerPosition = 4;
+//            if(playerScore>=1000){
+//                playerPosition =1;
+//            }else if(playerScore>=500){
+//                playerPosition =2;
+//            }else if(playerScore>=100){
+//                playerPosition=3;
+//            }
+//            return playerPosition;
         }
-        return 4;
-    }
 }
