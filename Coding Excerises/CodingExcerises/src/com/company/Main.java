2d0d1a39f532d74646d129c126c448e76e673334
tanishqsindhu <hisar.champion@gmail.com>
2022-01-02 22:32:11 +0530

    Check Number Completed

--- Coding Excerises/CodingExcerises/src/com/company/Main.java
+++ Coding Excerises/CodingExcerises/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number to check ");
+        int number= sc.nextInt();
+        checkNubmber(number);
+    }
+    public static void checkNubmber(int number){
+        if (number>0){
+            System.out.println("Positive");
+        }else if (number<0){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
+        }
     }
 }
