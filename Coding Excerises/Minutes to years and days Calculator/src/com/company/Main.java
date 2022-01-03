698853eb7165d8b0584bbf3d61188eddeacf5bc3
tanishqsindhu <hisar.champion@gmail.com>
2022-01-04 01:23:38 +0530

    Minutes to Years and Days Calculator

---
+++ Coding Excerises/Minutes to years and days Calculator/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	printYearsAndDays(535700);
+    }
+
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        }else{
+            long years = minutes / 525600;
+            long days = (minutes % 525600) / 1440 ;
+
+
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
