68409081c21c53db30a61e9d249f7e09dc97da47
tanishqsindhu <hisar.champion@gmail.com>
2022-01-03 18:13:03 +0530

    Checkteen completed

--- Coding Excerises/TeenNumberChecker/src/com/company/Main.java
+++ Coding Excerises/TeenNumberChecker/src/com/company/Main.java
@@ -3,9 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	hasTeen(9,99,19);
     }
-    public static boolean hasTeen(){
-        return false;
+    public static boolean hasTeen(int num1,int num2,int num3){
+        if (num1>=13&&num1<=19||num2>=13&&num2<=19||num3<=19&&num3>=13){
+         return true;
+        }else{
+            return false;
+        }
+    }
+    public static boolean isTeen(int num1){
+        if (num1<=19&&num1>=13){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
