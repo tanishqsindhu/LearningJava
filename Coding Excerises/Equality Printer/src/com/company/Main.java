39bd22ecc63c01d1ab3a74a6cd3530ed1f0ca47a
tanishqsindhu <hisar.champion@gmail.com>
2022-01-04 01:32:00 +0530

    Compeleted Equality Printer

--- Coding Excerises/Equality Printer/src/com/company/Main.java
+++ Coding Excerises/Equality Printer/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	printEqual(1,2,1);
+    }
+
+    public static void printEqual(int num1,int num2,int num3) {
+        if(num1==num2&&num2==num3&&num1>0&&num2>0&&num3>0){
+            System.out.println("All numbers are equal");
+        }else if (num1!=num2&&num2!=num3&&num1!=num3){
+            System.out.println("All numbers are different");
+        }else if (num1<0||num2<0||num3<0){
+            System.out.println("Invalid Value");
+        }else{
+            System.out.println("Neither all are equal or different");
+        }
     }
 }
