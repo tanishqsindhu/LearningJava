4e7f6abbacb7849c54271208387f56d277875088
tanishqsindhu <hisar.champion@gmail.com>
2022-01-03 17:28:59 +0530

    EqualSumChecker

--- Coding Excerises/DecimalComparator/src/com/company/Main.java
+++ Coding Excerises/DecimalComparator/src/com/company/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	areEqualByThreeDecimalPlaces(-3.1756,-3.175);
     }
-    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
-        
+    public static boolean areEqualByThreeDecimalPlaces(double myFirstDouble, double mySecondDouble){
+            int myFirstCheck = (int)(myFirstDouble * 1000);
+            int mySecondCheck = (int)(mySecondDouble * 1000);
+            if(myFirstCheck - mySecondCheck == 0){
+
+                return true;
+            } else {
+                return false;
+            }
     }
 }
