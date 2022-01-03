dcf67ee51bd986302c32c2448e743a6c47613d82
tanishqsindhu <hisar.champion@gmail.com>
2022-01-03 17:48:55 +0530

    EqualSum Completed

--- Coding Excerises/EqualSumChecker/src/com/company/Main.java
+++ Coding Excerises/EqualSumChecker/src/com/company/Main.java
@@ -5,4 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+    public static boolean hasEqualSum(int num1,int num2,int num3){
+        if (num1+num2 == num3){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
