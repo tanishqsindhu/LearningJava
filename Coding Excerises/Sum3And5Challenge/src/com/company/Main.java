04767f0ba7c5fb6a738cfb786df2ac4e9fc7c91a
tanishqsindhu <hisar.champion@gmail.com>
2022-01-05 18:16:09 +0530

    Completed Sum3And5Challenge

--- Coding Excerises/Sum3And5Challenge/src/com/company/Main.java
+++ Coding Excerises/Sum3And5Challenge/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int count = 0;
+        int sum=0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+                sum += i;
+                count++;
+            }
+            if (count == 5) {
+                System.out.println(sum);
+                break;
+            }
+        }
     }
 }
