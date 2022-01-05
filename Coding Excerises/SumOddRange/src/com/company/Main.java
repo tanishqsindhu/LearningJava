72285ec8c876f38731c0369299273c1cf500b770
tanishqsindhu <hisar.champion@gmail.com>
2022-01-05 18:37:57 +0530

    OddSum Challenge Completed

--- Coding Excerises/SumOddRange/src/com/company/Main.java
+++ Coding Excerises/SumOddRange/src/com/company/Main.java
@@ -2,7 +2,28 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+    sumOdd(1,100);
+    }
+    public static boolean isOdd(int number){
+        if (number>0){
+            if(number%2!=0){
+                return true;
+            }
+        }
+            return false;
+    }
+    public static int sumOdd(int start,int end){
+        int sum=0;
+        if (start<0||end<0||end<start) {
+          return -1;
+        }
+        for (int i=start; i<=end; i++) {
+            if (isOdd(i)){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        return sum;
     }
 }
