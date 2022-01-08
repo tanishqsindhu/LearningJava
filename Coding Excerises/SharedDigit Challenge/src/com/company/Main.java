f25127d700c8e2bc38d7e212e4bf64013a02b827
tanishqsindhu <hisar.champion@gmail.com>
2022-01-08 14:53:26 +0530

    SharedDigit Challenge initial Commit

---
+++ Coding Excerises/SharedDigit Challenge/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.lang.instrument.IllegalClassFormatException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	hasSharedDigit(12,23);
+    }
+    public static boolean hasSharedDigit(int firstRange,int lastRange){
+        if(firstRange<9&&lastRange>100){
+            return false;
+        }
+        while (firstRange){
+            if ()
+        }
+        return false;
+    }
+}
