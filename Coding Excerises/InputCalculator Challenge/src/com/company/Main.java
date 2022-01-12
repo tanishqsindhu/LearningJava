3b2d414809b1676f11eff501aa676f4b89cfb1ee
tanishqsindhu <hisar.champion@gmail.com>
2022-01-12 19:03:19 +0530

    Completed InputCalculator

--- Coding Excerises/InputCalculator Challenge/src/com/company/Main.java
+++ Coding Excerises/InputCalculator Challenge/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        inputThenPrintSumAndAverage();
+    }
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum=0,count=0;
+        while(true){
+            System.out.println("Enter Number");
+            boolean isNumber = scanner.hasNextInt();
+            if(isNumber){
+                int number = scanner.nextInt();
+                sum+=number;
+                count++;
+            }else{
+                break;
+            }
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum/count));
+        scanner.close();
     }
 }
---
+++ Coding Excerises/PaintJobChallenge/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static int getBucketCount(double width,double height,double areaPerBucket,double extraBucket){
+
+    }
+}
