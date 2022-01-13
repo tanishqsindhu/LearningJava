5ebc60a316214d5db1fa7a0c0a7f02d5f9ddf523
tanishqsindhu <hisar.champion@gmail.com>
2022-01-13 19:08:33 +0530

    Completed PaintJob Challenge

--- Coding Excerises/PaintJobChallenge/src/com/company/Main.java
+++ Coding Excerises/PaintJobChallenge/src/com/company/Main.java
@@ -1,11 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	getBucketCount(3.4,2.1,1.5,2);
     }
     public static int getBucketCount(double width,double height,double areaPerBucket,double extraBucket){
+            if(width<=0||height<=0||areaPerBucket<=0||extraBucket<0){
+                System.out.println("Invalid Value");
+                return -1;
+            }
+            return (int) Math.ceil((((width*height)/areaPerBucket)-extraBucket));
 
+    }
+    public static int getBucketCount(double width,double height,double areaPerBucket){
+        if(width<=0||height<=0||areaPerBucket<=0){
+            System.out.println("Invalid Value");
+            return -1;
+        }
+        return (int) Math.ceil(((width*height)/areaPerBucket));
+    }
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area<=0||areaPerBucket<=0){
+            System.out.println("Invalid Value");
+            return -1;
+        }
+        return (int) Math.ceil((area/areaPerBucket));
     }
 }
