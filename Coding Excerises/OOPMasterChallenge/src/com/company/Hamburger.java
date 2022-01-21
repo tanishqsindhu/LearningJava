e144d88a074ff18f292520e881450a967d73f0f8
tanishqsindhu <hisar.champion@gmail.com>
2022-01-22 01:41:33 +0530

    Completed Bill's Burgers Completed

--- Coding Excerises/OOPMasterChallenge/src/com/company/Hamburger.java
+++ Coding Excerises/OOPMasterChallenge/src/com/company/Hamburger.java
@@ -1,31 +1,51 @@
 package com.company;
 
-public class Hamburger() {
-    private String name,addition1Name,addition2Name,addition3Name,addition4Name;
+public class Hamburger {
+    private String name;
     private String meat;
+    private double price;
     private String breadRollType;
-    private double price,addition1Price,addition2Price,addition3Price,addition4Price;
+
+    private String addition1Name;
+    private String addition2Name;
+    private String addition3Name;
+    private String addition4Name;
+
+    private double addition1Price;
+    private double addition2Price;
+    private double addition3Price;
+    private double addition4Price;
 
     public Hamburger(String name, String meat, double price, String breadRollType) {
         this.name = name;
         this.meat = meat;
-        this.breadRollType = breadRollType;
         this.price = price;
+        this.breadRollType = breadRollType;
     }
 
-    public void addHamburgerAddition1(String addition1Name, double addition1Price) {
-        this.addition1Name = addition1Name;
-        this.addition1Price = addition1Price;
-    }
-    public void addHamburgerAddition2(String addition2Name, double addition2Price){
-        this.addition2Name=addition2Name;
+    public void addHamburgerAddition1(String name, double price) {
+        addition1Price = price;
+        addition1Name = name;
     }
 
-    public void addHamburgerAddition3(String addition3Name, double addition3Price) {
-        this.addition3Name = addition3Name;
-        this.addition3Price = addition3Price;
+    public void addHamburgerAddition2(String name, double price) {
+        addition2Price = price;
+        addition2Name = name;
     }
-    public double itemizeHamburger(){
-    return price+addition3Price+addition2Price+addition1Price;
+
+    public void addHamburgerAddition3(String name, double price) {
+        addition3Price = price;
+        addition3Name = name;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        addition4Price = price;
+        addition4Name = name;
+    }
+
+    public double itemizeHamburger() {
+        System.out.println("One order of " + name + " on " + breadRollType + " with " + addition1Name + " " + addition2Name + " " + addition3Name + " " + addition4Name);
+        return (price + addition1Price + addition2Price + addition3Price + addition4Price);
     }
 }
+
