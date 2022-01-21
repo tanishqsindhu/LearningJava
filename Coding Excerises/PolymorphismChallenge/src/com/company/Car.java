11be75946e9d9ed19daf10ea1e79b166daeabc22
tanishqsindhu <hisar.champion@gmail.com>
2022-01-21 23:40:14 +0530

    Car

---
+++ Coding Excerises/PolymorphismChallenge/src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+    }
+}
