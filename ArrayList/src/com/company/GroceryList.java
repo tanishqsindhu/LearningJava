dea3efe27d94728885f320949ca19b91eeeecd82
tanishqsindhu <hisar.champion@gmail.com>
2022-01-24 13:54:48 +0530

    Updated GroceryList

---
+++ ArrayList/src/com/company/GroceryList.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private int [] numbers;
+    private ArrayList<String> groceryList=new ArrayList<String>();
+    public void addGroceryItems(){
+        
+    }
+}
