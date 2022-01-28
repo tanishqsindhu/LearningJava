c5ee0be908f46e2c06f02a40b7e7af5c5a9302fc
tanishqsindhu <hisar.champion@gmail.com>
2022-01-28 17:43:15 +0530

    Updated MobilePhone.java

--- Coding Excerises/MobilePhone Challenge/src/com/company/MobilePhone.java
+++ Coding Excerises/MobilePhone Challenge/src/com/company/MobilePhone.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = myNumber;
+    }
+    public boolean addNewContact(String contact){
+        if (contact){
+            return false;
+        }
+        return true;
+    }
 }
