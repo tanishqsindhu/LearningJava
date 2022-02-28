10ae8792386d560188dd50c432b63818a183535d
tanishqsindhu <hisar.champion@gmail.com>
2022-02-28 20:46:48 +0530

    Interface Part 2 completed

--- Interfaces/src/com/company/Main.java
+++ Interfaces/src/com/company/Main.java
@@ -9,5 +9,10 @@ public class Main {
         timsPhone.callPhone(123456);
         timsPhone.answer();
 
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
+
     }
 }
