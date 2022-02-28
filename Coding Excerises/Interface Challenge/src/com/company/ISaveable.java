33b6ce056c2e7f8f51ee0944fdffd23c402b2c98
tanishqsindhu <hisar.champion@gmail.com>
2022-02-28 21:18:35 +0530

    Interface challenge Completed

--- Coding Excerises/Interface Challenge/src/com/company/ISaveable.java
+++ Coding Excerises/Interface Challenge/src/com/company/ISaveable.java
@@ -1,4 +1,8 @@
 package com.company;
 
+import java.util.List;
+
 public interface ISaveable {
+    List<String> write();
+    Void read(List<String> list);
 }
--- Coding Excerises/Interface Challenge/src/com/company/Monster.java
+++ Coding Excerises/Interface Challenge/src/com/company/Monster.java
@@ -1,4 +1,63 @@
 package com.company;
 
-public class Monster {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values= new ArrayList<>();
+        values.add(0,name);
+        values.add(1,""+hitPoints);
+        values.add(2,""+strength);
+        return values;
+    }
+
+    @Override
+    public Void read(List<String> list) {
+        if (list!=null&&list.size()>0){
+            this.name=list.get(0);
+            this.hitPoints=Integer.parseInt(list.get(1));
+            this.strength=Integer.parseInt(list.get(2));
+        }
+        return null;
+    }
+    @Override
+    public String toString(){
+        return "{name='"+name+"', hitPoints="+hitPoints+", strength="+strength+"}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
