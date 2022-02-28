9f7e75fcc7ed0099bad5b6b994ecbba5d2202725
tanishqsindhu <hisar.champion@gmail.com>
2022-02-28 19:36:17 +0530

    Interfaces Part 1 completed

--- Interfaces/src/com/company/Main.java
+++ Interfaces/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+
     }
 }
