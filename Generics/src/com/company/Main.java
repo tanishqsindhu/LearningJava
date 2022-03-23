964a526de0a858e86a7244073f1befe5defbde1a
tanishqsindhu <hisar.champion@gmail.com>
2022-03-23 22:16:58 +0530

    Completed Generics Information

--- Generics/src/com/company/Main.java
+++ Generics/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(1);
+        number.add(2);
+       // number.add("Tanishq");
+        number.add(3);
+        number.add(4);
+        number.add(5);
+        doubleNumber(number);
+    }
+    public static void doubleNumber(ArrayList<Integer> numbers){
+        for(Integer i : numbers ){
+            System.out.println(i*2);
+        }
     }
 }
