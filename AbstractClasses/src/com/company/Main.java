b8b1e12d748347c7a1ff551d8057568457f1e63e
tanishqsindhu <hisar.champion@gmail.com>
2022-03-23 18:29:53 +0530

    Completed Part1

--- AbstractClasses/src/com/company/Main.java
+++ AbstractClasses/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
-public class Main {
+    public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+        public static void main(String[] args) {
+            Dog dog = new Dog("Yorkie");
+            dog.breathe();
+            dog.eat();
+
+            Parrot parrot = new Parrot("Australian ringneck");
+            parrot.breathe();
+            parrot.eat();
+            parrot.fly();
+
+            Penguin penguin = new Penguin("Emperor");
+            penguin.fly();
+        }
     }
-}
